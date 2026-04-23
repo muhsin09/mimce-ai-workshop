813da4d9040811288a32b93a003db11aca2cd357
Ahmed Enis ERKAYA <enis.erkaya@tubitak.gov.tr>
2026-04-23 12:31:18 +0300

    bismillah

---
+++ src/main/java/com/mimce/workshop/MimceWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.mimce.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimceWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimceWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mimce/workshop/MimceWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mimce.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimceWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
